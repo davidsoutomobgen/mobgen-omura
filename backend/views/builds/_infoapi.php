d41720d20b282a9200d95b492bfe968390c3b835
dsoutomobgen <david.souto@mobgen.com>
2017-02-03 10:25:01 +0100

    Change id on element to fixed error with file input

--- backend/views/builds/_infoapi.php
+++ backend/views/builds/_infoapi.php
@@ -2,14 +2,13 @@
     <div class="box-header with-border">
         <h3 class="box-title">API Keys</h3>
         <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
-            </button>
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
             <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
         </div>
     </div>
 
     <div class="box-body">
-        <table id="w0" class="table table-striped table-bordered detail-view">
+        <table id="w0_apis" class="table table-striped table-bordered detail-view">
             <tbody>
             <tr><th>ID</th><td><?php echo $otaProject->id; ?></td></tr>
             <tr><th>API Project</th><td><?php echo $otaProject->proAPIKey; ?></td></tr>
