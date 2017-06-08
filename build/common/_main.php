7138264ecb42a6dd1dcbc8e9923a599b2df2adcd
Miguel Rodr?guez <miguel.rodriguez@mobgen.com>
2017-06-08 13:01:41 +0200

    Solved timezone problem

--- build/common/_main.php
+++ build/common/_main.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'timeZone' => 'Europe/Amsterdam',
     'bootstrap' => ['devicedetect'],
     'components' => [
         'db' => [
